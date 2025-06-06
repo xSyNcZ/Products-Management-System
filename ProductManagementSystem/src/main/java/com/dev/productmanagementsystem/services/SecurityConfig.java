d57e4f72dffa8dbfe4877e6b3c58c2314585d21e
xSyNcZ <dragonmatik@gmail.com>
2025-06-06 15:10:05 +0200

    Frontend Placeholders added

--- ProductManagementSystem/src/main/java/com/dev/productmanagementsystem/services/SecurityConfig.java
+++ ProductManagementSystem/src/main/java/com/dev/productmanagementsystem/services/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.dev.productmanagementsystem.services;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -10,6 +11,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();  // Użycie BCryptPasswordEncoder do kodowania haseł
+        return new BCryptPasswordEncoder();
     }
 }
